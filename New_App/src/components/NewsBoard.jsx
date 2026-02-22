4702f7b934b49285c721125ed9cf4c7dee1ac1b6
SoumyaMadishetti17 <soumyamadishettimsd17@gmail.com>
2026-02-23 00:39:30 +0530

    stage done

--- New_App/src/components/NewsBoard.jsx
+++ New_App/src/components/NewsBoard.jsx
@@ -1,53 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 import NewsItem from './NewsItem';
 import '../App.css'
 
-const NewsBoard = ({ category }) => {
-  const [articles, setArticles] = useState([]);
+const NewsBoard = ({category}) => {
+    const [articles,setArticles]=useState([]);
+    useEffect(()=>{
+        let url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
 
-  useEffect(() => {
-    const apiKey = import.meta.env.VITE_API_KEY;
-
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`;
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        if (data.articles) {
-          setArticles(data.articles);
-        } else {
-          setArticles([]); // prevent crash
-        }
-      })
-      .catch(err => {
-        console.error("API Error:", err);
-        setArticles([]);
-      });
-  }, [category]);
-
+        fetch(url).then(res=>res.json()).then(data=>setArticles(data.articles))
+    },[category])
   return (
-    <div style={{ backgroundColor: 'gray' }}>
-      <h2 className='text-center py-3'>
-        Latest <span className='badge bg-danger'>News</span>
-      </h2>
-
-      <div className='news-container'>
-        {articles.length > 0 ? (
-          articles.map((news, index) => (
-            <NewsItem
-              key={index}
-              title={news.title}
-              description={news.description}
-              src={news.urlToImage}
-              url={news.url}
-            />
-          ))
-        ) : (
-          <p className="text-center">No news available</p>
-        )}
-      </div>
+    <div style={{backgroundColor:'gray'}}>
+        <h2 className='text-center py-3 '>Latest <span className='badge bg-danger'>News</span></h2>
+        <div className='news-container'>
+            {articles.map((news,index)=>{
+                return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+            })}
+        </div>
+        
     </div>
-  );
+  )
 }
 
-export default NewsBoard;
+export default NewsBoard
